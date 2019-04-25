7bbd5122d2c4bcf82d0da097241900baecb10aa4
Brian Tweed <btweed@simplyzesty.com>
2019-04-25 14:30:19 +0100

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,7 @@ let tags = ['project_author', 'project_title', 'template_name', 'favicon_name', 
  */
 const ucwords = (string) => {
     return string.toLowerCase()
-        .split(/[ -_]+/)
+        .split(/[\s-_]+/)
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
 };
