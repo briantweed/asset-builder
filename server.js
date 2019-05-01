4130a4bace70b0ca2a0e912012f4db029e2cff99
brtweed@outlook.com <brtweed@outlook.com>
2019-05-01 20:53:39 +0100

    no message

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const parse = require('body-parser');
 const express = require('express');
 const { exec } = require('child_process');
-const { spawn } = require('child_process');
 
 const app = express();
 
 app.use(parse.json());
 app.use(parse.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -18,11 +18,11 @@ app.use(function (req, res, next) {
 });
 
 
-app.listen(3000, function () {
+app.listen(3000, function (str) {
+    console.log(str);
     if (!fs.existsSync('./names.json')) {
         fs.writeFile('./names.json', '[]', function (err) {
             if (err) throw err;
-            console.log('names.json created');
         });
     }
     exec('gulp setup');
@@ -32,11 +32,9 @@ app.listen(3000, function () {
 
 
 app.post('/send', function(req, res){
-    const child  = spawn('gulp', [req.body.command], {});
-    child.stdout.on('data', function(data) {
-
+    exec(req.body.command, function() {
+        res.json({success : req.body.command + " task ran successfully", status : 200});
     });
-    res.json({success : 'done', status : 200});
 });
 
 
@@ -48,7 +46,6 @@ app.post('/names', function(req, res){
 
 
 app.post('/create', function(req, res){
-    console.log(req.body.command);
     if(req.body.command.length) {
         exec('gulp template --name "' + req.body.command + '"', function() {
             res.json({success : req.body.command + " page created", status : 200});
